304715f2da7cd322bd853ed30130e0fb3013f37a
idrissAbbou <idrisskapan53@gmail.com>
2020-08-24 23:02:52 +0100

    add some more data

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 class CovidStatus {
   constructor() {
     this.contries = null;
+    this.timeLine = null;
   }
 
   async loadData() {
@@ -16,7 +17,19 @@ class CovidStatus {
     this.contries = json.response;
   }
 
+  async loadCountryTimeLine() {
+    const api = await fetch(
+      "https://api.thevirustracker.com/free-api?countryTimeline=MA", {
+        method: "GET",
+      }
+    );
+    const response = await api.json();
+    this.timeLine = response.timelineitems[0];
+    return response.timelineitems[0];
+  }
+
   findCountryData(countryName) {
+    countryName = countryName.charAt(0).toUpperCase() + countryName.slice(1);
     const country = this.contries.filter((c) => c.country == countryName);
     return country[0];
   }
@@ -24,8 +37,14 @@ class CovidStatus {
 
 const covidStatus = new CovidStatus();
 
+//after page loead
+//load the data
+//show morocco status
 (async () => {
   await covidStatus.loadData();
+  const cn = covidStatus.findCountryData("Morocco");
+  displayData(cn);
+  displayTimeLineTable(await covidStatus.loadCountryTimeLine());
 })();
 
 //get dom elements
@@ -44,13 +63,20 @@ const lastUpdate = document.querySelector(".last-update");
 btn.addEventListener("click", function () {
   const cn =
     countryName.value.charAt(0).toUpperCase() + countryName.value.slice(1);
-  const countryData = covidStatus.findCountryData("Morocco");
+  const countryData = covidStatus.findCountryData(cn);
   displayData(countryData);
+  displayTimeLineTable(covidStatus.loadCountryTimeLine());
+});
+
+document.querySelector(".show_table").addEventListener("click", function (e) {
+  const table = document.querySelector(".dataTable");
+  table.classList.toggle('hidden');
+  table.classList.toggle('show');
+
 });
 
 function displayData(countryData) {
   const countryName = document.querySelector(".country-name");
-  console.log(countryData);
   countryName.innerHTML = countryData.country;
   total.innerHTML = countryData.cases.total;
   active.innerHTML = countryData.cases.active;
@@ -61,4 +87,99 @@ function displayData(countryData) {
   totalTests.innerHTML = countryData.tests.total;
   population.innerHTML = countryData.population;
   lastUpdate.innerHTML = countryData.day;
+}
+
+function displayTimeLineTable(jsonData) {
+  const timelineTable = document.querySelector(".dataTable");
+  for (let date in jsonData) {
+    if (!jsonData.hasOwnProperty(date) || date == "stat") continue;
+
+    const tr = document.createElement("tr");
+    const tdDate = document.createElement("td");
+    const tdcases = document.createElement("td");
+    const tddeath = document.createElement("td");
+    const tdtotalCases = document.createElement("td");
+    const tdtotalRecov = document.createElement("td");
+    const tdtotalDeaths = document.createElement("td");
+    tdDate.innerText = date;
+    tdcases.innerText = jsonData[date].new_daily_cases;
+    tddeath.innerText = jsonData[date].new_daily_deaths;
+    tdtotalCases.innerText = jsonData[date].total_cases;
+    tdtotalRecov.innerText = jsonData[date].total_recoveries;
+    tdtotalDeaths.innerText = jsonData[date].total_deaths;
+    tr.appendChild(tdDate);
+    tr.appendChild(tdcases);
+    tr.appendChild(tddeath);
+    tr.appendChild(tdtotalCases);
+    tr.appendChild(tdtotalRecov);
+    tr.appendChild(tdtotalDeaths);
+    timelineTable.appendChild(tr);
+  }
+}
+
+//google chart
+google.charts.load("current", {
+  packages: ["corechart"]
+});
+google.charts.setOnLoadCallback(drawChart);
+
+async function drawChart() {
+  const arr = [];
+  const timeLine = await covidStatus.loadCountryTimeLine();
+  arr.push(["date", "total cases", "total deaths"]);
+  for (let key in timeLine) {
+    const cases = timeLine[key].total_cases;
+    const deaths = timeLine[key].total_deaths;
+    const val = [key, cases, deaths];
+    arr.push(val);
+  }
+
+  var data = google.visualization.arrayToDataTable(arr);
+
+  var options = {
+    title: "total cases per day",
+    legend: {
+      position: "right"
+    },
+    colors: ["black", "red"],
+  };
+
+  var chart = new google.visualization.LineChart(
+    document.getElementById("curve_chart")
+  );
+
+  chart.draw(data, options);
+}
+
+google.charts.load("current", {
+  packages: ["corechart"]
+});
+google.charts.setOnLoadCallback(drawChart);
+
+async function drawChart() {
+  const arr = [];
+  const timeLine = await covidStatus.loadCountryTimeLine();
+  arr.push(["date", "ااجمالي الحالات", "اجمالي الوفايات"]);
+  for (let key in timeLine) {
+    const cases = timeLine[key].total_cases;
+    const deaths = timeLine[key].total_deaths;
+    const val = [key, cases, deaths];
+    arr.push(val);
+  }
+
+  var data = google.visualization.arrayToDataTable(arr);
+
+  var options = {
+    title: "العدد الاجمالي للحالات و الوفايات",
+    legend: {
+      position: "right"
+    },
+    colors: ["black", "red"],
+  };
+
+  var chart = new google.visualization.LineChart(
+    document.getElementById("curve_chart_arab")
+  );
+
+  chart.draw(data, options);
 }
